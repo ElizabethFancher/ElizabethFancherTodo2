4fc4359ba718d541ad69a905e2ef136525440837
Elizabeth Fancher <elizabethfancher@hotmail.com>
2019-09-21 20:18:03 -0400

    Restarting

---
+++ ElizabethFancherTodo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ElizabethFancherTodo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
